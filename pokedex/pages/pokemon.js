7c1d00d7a57d8637c97397eaeaf394e5133cfe63
Daniel Neves Coelho <danielcoelho@pop-os.localdomain>
2021-08-14 15:30:47 -0300

    Progresso no CSS das paginas especificas

--- pokedex/pages/pokemon.js
+++ pokedex/pages/pokemon.js
@@ -5,24 +5,47 @@ import TopBar from '../components/topBar';
 export default function pokemon({poke}) {
     return (
         <body>
-            <TopBar/>
-            <a> 
-                <img width="179px" height="179px" src={poke.image} alt={poke.name}/>
-                <p> Name: {poke.name}</p>
-                <p> ID: {poke.id} </p>
-                <p> Height: {poke.height/10}m </p>
-                <p> Base EXP: {poke.base_experience}</p>
-                <p> Weight: {poke.weight/10}kg </p>
-                Types: {poke.types.map((type, id) => (
-                    <p key = {id}>{type.type.name}</p>
-                ))}
-                Abilities: {poke.abilities.map((abilities, id) => (
-                    <p key = {id}>{abilities.ability.name}</p>
-                ))}
-                Stats: {poke.stats.map((stats, id) => (
-                    <p key = {id}> {stats.stat.name} = {stats.base_stat}</p>
-                ))}
-            </a>
+            <header><TopBar/></header>
+            <main className={styles.specificContainer}>   
+                <div className={styles.column1}>
+                    <div className={styles.imageBox}>
+                        <img src={poke.image} alt={poke.name}/>
+                    </div>
+                    <div className={styles.statistics}>
+                        <div className={styles.gridStatistics}>
+                            
+                        </div>
+                    </div>
+                    <strong><a>
+                    {poke.stats.map((stats, id) => (
+                        <a key = {id}> {stats.stat.name} </a>))}
+                    </a></strong>
+                </div>
+
+
+                <div className={styles.column2}>
+                    <h1> Name: {poke.name}</h1>
+                    <div className={styles.caseBox}>
+                        <div className={styles.dataBox}>              
+                            <p> ID: {poke.id} </p>
+                            <p> Height: {poke.height/10}m </p>
+                            <p> Base EXP: {poke.base_experience}</p>
+                            <p> Weight: {poke.weight/10}kg </p>
+                            {poke.abilities.map((abilities, id) => (
+                                <p key = {id}>{abilities.ability.name}</p>
+                            ))}
+                        </div> 
+                    </div>
+                    <section>
+                        <h2>Type</h2>
+                        {poke.types.map((type, id) => (
+                            <a key = {id}>{type.type.name} |</a>
+                        ))}
+                        <h2>Weakness</h2>
+                        
+                    </section>
+                </div>
+            </main>
         </body>
     );
 }
@@ -43,4 +66,4 @@ export async function getServerSideProps({query}) {
             notFound: true
           }
         }
-}   
+} 
